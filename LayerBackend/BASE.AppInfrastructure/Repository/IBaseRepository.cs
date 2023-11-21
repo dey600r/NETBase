0022cc83230f407720811c1bc2872071b3513e2c
dey125r <dey125r@gmail.com>
2023-11-21 20:12:50 +0100

    Start Ibaserepository

---
+++ LayerBackend/BASE.AppInfrastructure/Repository/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using BASE.AppInfrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BASE.AppInfrastructure.Repository
+{
+	public interface IBaseRepository<TEntity, TId> where TEntity : class, IBaseEntity<TId>, new()
+													where TId : struct
+	{
+
+	}
+}
