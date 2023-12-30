b6accb0fd74046ac02a68ceb99a4eb1605135fc6
dey125r <dey125r@gmail.com>
2023-12-30 21:41:49 +0100

    Add login

--- Frontend/angular-frontend/src/app/pages/security/login/login.component.ts
+++ Frontend/angular-frontend/src/app/pages/security/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login.component',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  /* AQUI DEFINIMOS LA TEMATICA DE NUESTRA IMAGEN*/
+  styleImage = 'rain';
 
+  form: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor(private formBuilder: FormBuilder,
+    private router: Router){
+  }
+
+  ngOnInit(): void {}
+
+  login(event: Event): any {
+    this.router.navigateByUrl('/home');
+    // event.preventDefault();
+    // if (this.form.valid) {
+    //   const value = this.form.value;
+    //   console.log(`'%c'USER: ${value.email} - PASSWORD: ${value.password}`, 'background: #222; color: #bada55');
+    // }
+  }
 }
--- Frontend/angular-frontend/src/app/pages/security/login/login.module.ts
+++ Frontend/angular-frontend/src/app/pages/security/login/login.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
-
-
+import { MaterialModule } from '../../material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,12 @@ import { LoginRoutingModule } from './login-routing.module';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   exports: [
     LoginComponent
