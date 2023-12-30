be7db3f64afa19cca000a9118d515fcf4b91b37c
dey125r <dey125r@gmail.com>
2023-12-30 20:52:01 +0100

    Add routing to login and home

--- Frontend/angular-frontend/src/app/app.routes.ts
+++ Frontend/angular-frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        loadChildren: () => import('./pages/pages.module').then(mod => mod.PagesModule)
+    },
+    {
+        path: 'login',
+        loadChildren: () => import('./pages/security/login/login.module').then(mod => mod.LoginModule)
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
+      }
+];
---
+++ Frontend/angular-frontend/src/app/pages/pages.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { PagesRoutingModule } from './pages-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    PagesRoutingModule
+  ],
+  exports: [
+    HomeComponent
+  ]
+})
+export class PagesModule { }
