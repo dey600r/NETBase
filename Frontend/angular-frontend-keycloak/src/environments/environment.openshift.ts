71d008a648e32930103a5fa29baabe5b3a280e86
dey125r <dey125r@gmail.com>
2024-05-05 18:35:17 +0200

    Openshift

--- Frontend/angular-frontend-keycloak/src/environments/environment.openshift.ts
+++ Frontend/angular-frontend-keycloak/src/environments/environment.openshift.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://dey-backend-microservices.apps-crc.testing:3400',
+    apiUrl: 'https://dey-backend-microservices.apps-crc.testing',
     keycloak: {
       enable: false,
       url: 'http://localhost:8180',
