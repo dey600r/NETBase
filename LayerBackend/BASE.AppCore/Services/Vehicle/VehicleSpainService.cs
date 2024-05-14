9aad5bf93ae4d1225feeac7a9a7e67bde9e4996f
david.d.jimenez-galiana.external <david.d.jimenez-galiana.external@airbus.com>
2024-05-14 09:35:50 +0200

    Test use context tracking data

--- LayerBackend/BASE.AppCore/Services/Vehicle/VehicleSpainService.cs
+++ LayerBackend/BASE.AppCore/Services/Vehicle/VehicleSpainService.cs
@@ -13,40 +13,53 @@ namespace BASE.AppCore.Services
 		}
 
 		public IEnumerable<VehicleModel> GetVehicles() {
-		
-			_baseRepository.Add(new List<Vehicle>()
-			{
-				new Vehicle
-				{
-					Brand = "Yamaha",
-					Model = "R6",
-					Km = 100000,
-					Year = 2006,
-					DateKms = DateTime.UtcNow,
-					KmsPerMonth = 600,
-					Active = true,
-					VehicleTypeId = 1,
-					//VehicleType = new VehicleType() { Id = 1 },
-					ConfigurationId = 1,
-					//Configuration = new Configuration() { Id = 1 }
-				},
-				new Vehicle
-				{
-					Brand = "Kawasaki",
-					Model = "Ninja 1000 sx",
-					Km = 30000,
-					Year = 2021,
-					DateKms = DateTime.UtcNow,
-					KmsPerMonth = 600,
-					Active = true,
-					VehicleTypeId = 1,
-					//VehicleType = new VehicleType() { Id = 1 },
-					ConfigurationId = 1,
-					//Configuration = new Configuration() { Id = 1 }
-				}
-			});
 
-			return _baseRepository.GetAll(x => x.Brand == "Yamaha").Select(x => _mapper.Map<VehicleModel>(x)).ToList();
+            //var results = _baseRepository.Add(new List<Vehicle>()
+            //{
+            //	new Vehicle
+            //	{
+            //		Brand = "Yamaha",
+            //		Model = "R6",
+            //		Km = 100000,
+            //		Year = 2006,
+            //		DateKms = DateTime.UtcNow,
+            //		KmsPerMonth = 600,
+            //		Active = true,
+            //		VehicleTypeId = 1,
+            //		//VehicleType = new VehicleType() { Id = 1 },
+            //		ConfigurationId = 1,
+            //		//Configuration = new Configuration() { Id = 1 }
+            //	},
+            //	new Vehicle
+            //	{
+            //		Brand = "Kawasaki",
+            //		Model = "Ninja 1000 sx",
+            //		Km = 30000,
+            //		Year = 2021,
+            //		DateKms = DateTime.UtcNow,
+            //		KmsPerMonth = 600,
+            //		Active = true,
+            //		VehicleTypeId = 1,
+            //		//VehicleType = new VehicleType() { Id = 1 },
+            //		ConfigurationId = 1,
+            //		//Configuration = new Configuration() { Id = 1 }
+            //	}
+            //});
+
+            IEnumerable<VehicleModel> results = GetAll(x => x.Brand == "Yamaha" || x.Brand == "Kawasaki").ToList();
+
+            //IEnumerable<Vehicle> results = _baseRepository.GetAll(x => x.Brand == "Yamaha" || x.Brand == "Kawasaki").ToList();
+
+            results.ElementAt(0).Brand = "Honda";
+            results.ElementAt(0).Brand = "Triumph";
+
+            //results = _baseRepository.Update(results);
+
+            return Update(results);
+
+			//return results.Select(x => _mapper.Map<VehicleModel>(x)).ToList();
+
+            //return _baseRepository.GetAll(x => x.Brand == "Yamaha").Select(x => _mapper.Map<VehicleModel>(x)).ToList();
 		}
 	}
 }
