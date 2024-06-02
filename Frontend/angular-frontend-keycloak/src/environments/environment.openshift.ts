fe418b2b41ce9f7f5ef7d5b049277dec5d7e94db
dey125r <dey125r@gmail.com>
2024-06-02 10:33:42 +0200

    Config microservices with keycloak on openshift

--- Frontend/angular-frontend-keycloak/src/environments/environment.openshift.ts
+++ Frontend/angular-frontend-keycloak/src/environments/environment.openshift.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: true,
     apiUrl: 'https://microservice-gateway-api.apps-crc.testing',
     keycloak: {
-      enable: false,
-      url: 'http://localhost:8180',
+      enable: true,
+      url: 'https://keycloak-microservices.apps-crc.testing',
       realm: 'microservices',
       clientId: 'frontend',
       onLoad: 'login-required',
