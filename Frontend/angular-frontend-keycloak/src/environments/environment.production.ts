9a225434a7d2ddeaa791aeea3dd94535f1542f37
dey125r <dey125r@gmail.com>
2024-07-14 12:26:04 +0200

    create tags for jwt and keycloak setting on docker

--- Frontend/angular-frontend-keycloak/src/environments/environment.production.ts
+++ Frontend/angular-frontend-keycloak/src/environments/environment.production.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'http://localhost:3400',
   keycloak: {
-    enable: true,
+    enable: false,
     url: 'http://localhost:8180',
     realm: 'microservices',
     clientId: 'frontend',
