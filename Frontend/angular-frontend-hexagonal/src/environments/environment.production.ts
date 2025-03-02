4a86b91b3aa5fd0da8725303a3443ef06aea2ee8
dey125r <dey125r@gmail.com>
2025-03-02 11:48:34 +0100

    Add hexagonal ssr to docker container

--- Frontend/angular-frontend-hexagonal/src/environments/environment.production.ts
+++ Frontend/angular-frontend-hexagonal/src/environments/environment.production.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'http://localhost:3400',
   keycloak: {
-    enable: false,
+    enable: true,
     url: 'http://localhost:8180',
     realm: 'microservices',
     clientId: 'frontend',
