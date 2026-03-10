d6e1ed00e23fc41bd89bddcfaaadc2a5849d7d1c
dey125r <dey125r@gmail.com>
2026-03-10 18:35:06 +0100

    fix setting microservice

--- Backend/MicroservicesBackend/Microservice.SettingApi/src/server/core/middlewares/config.ts
+++ Backend/MicroservicesBackend/Microservice.SettingApi/src/server/core/middlewares/config.ts
@@ -1,5 +1,4 @@
 import * as config from '../../../assets/config.json';
-import * as configOCP from '../../../assets/config.openshift.json';
 import * as configProd from '../../../assets/config.production.json';
 import { IConfigServer } from '../../core/models/utils/config-setting.model';
 
@@ -7,11 +6,6 @@ let cfg: IConfigServer = config as IConfigServer;
 if (process.env.NODE_ENV === 'production') {
     console.log('Production Mode...');
     cfg = configProd as IConfigServer;
-}
-
-if (process.env.NODE_ENV === 'openshift') {
-    console.log('Openshift Mode...');
-    cfg = configOCP as IConfigServer;
 }
 
 module.exports = { config: cfg };
