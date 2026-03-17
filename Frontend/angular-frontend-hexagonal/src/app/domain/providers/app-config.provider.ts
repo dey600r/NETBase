c2815bf8331d37478665becc63adef5c9c7545f0
dey125r <dey125r@gmail.com>
2026-03-17 18:36:15 +0100

    fix ssr

--- Frontend/angular-frontend-hexagonal/src/app/domain/providers/app-config.provider.ts
+++ Frontend/angular-frontend-hexagonal/src/app/domain/providers/app-config.provider.ts
@@ -8,26 +8,12 @@ export const KEYCLOAK_INSTANCE = new InjectionToken<Keycloak>('KEYCLOAK_INSTANCE
 
 export async function loadAppConfig(path: string) {
   console.log('Loading config from path:', path);
+  // ✅ Browser
+  const response = await fetch(`/${path}`);
 
-  const isBrowser = typeof window !== 'undefined';
-
-  if (isBrowser) {
-    // ✅ Browser
-    const response = await fetch(`/${path}`);
-
-    if (!response.ok) {
-      throw new Error('Error loading config file (browser)');
-    }
-
-    return await response.json();
-  } else {
-    // ✅ Node (SSR)
-    const { readFile } = await import('fs/promises');
-    const { join } = await import('path');
-
-    const filePath = join(process.cwd(), 'src/', path);
-    const file = await readFile(filePath, 'utf-8');
-
-    return JSON.parse(file);
+  if (!response.ok) {
+    throw new Error('Error loading config file (browser)');
   }
+
+  return await response.json();
 }
--- Frontend/angular-frontend-hexagonal/src/main.server.ts
+++ Frontend/angular-frontend-hexagonal/src/main.server.ts
@@ -1,8 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/adapters/ui/app.component';
 import { buildServerConfiguration } from './app/adapters/ui/app.config.server';
-import { loadAppConfig } from '@providers/index';
 import { environment } from '@environments/environment';
+
+export async function loadAppConfig(path: string) {
+  console.log('Loading config from path:', path);
+  // ✅ Node (SSR)
+  const { readFile } = await import('fs/promises');
+  const { join } = await import('path');
+
+  const filePath = join(process.cwd(), 'src/', path);
+  const file = await readFile(filePath, 'utf-8');
+
+  return JSON.parse(file);
+}
 
 export default async function bootstrap() {
 
